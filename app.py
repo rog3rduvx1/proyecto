3d791ff808ed40de845f2f6c39cf6391a983e5a7
rogerduvxi <sanitariumgervan@hotmail.com>
2025-04-10 11:34:37 -0500

    los mismos

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template
+
+app = Flask (__name__,static_folder ='assets')
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
